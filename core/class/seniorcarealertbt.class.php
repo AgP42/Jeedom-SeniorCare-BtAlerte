456173d89ed34784364d8d6a213fe0c822d4cbed
Agathe Pinel <agp.com@mailbox.org>
2020-03-25 21:53:13 +0100

    plugin terminé, reste a tester, debugger et écrire la doc

--- core/class/seniorcarealertbt.class.php
+++ core/class/seniorcarealertbt.class.php
@@ -26,19 +26,60 @@ class seniorcarealertbt extends eqLogic {
 
     /*     * ***********************Methode static*************************** */
 
+    public static function alertBtActionDelayed($_options) { // fonction appelée par les cron qui servent a reporter l'execution des actions d'alerte. Dans les options on trouve le eqLogic_id et 'action' qui lui meme contient tout ce qu'il faut pour executer l'action reportée, incluant le titre et message pour les messages
+
+      log::add('seniorcarealertbt', 'debug', 'Fct alertBtActionDelayed Appellée par le CRON - eqLogic_id : ' . $_options['eqLogic_id'] . ' - cmd : ' . $_options['action']['cmd'] . ' - action_label : ' . $_options['action']['action_label']);
+
+      $seniorcarealertbt = seniorcarealertbt::byId($_options['eqLogic_id']);
+
+      $seniorcarealertbt->execAction($_options['action']);
+
+    }
+
     public static function buttonAlert($_option) { // fct appelée par le listener des buttons d'alerte, n'importe quel bouton arrive ici
-      log::add('seniorcarealertbt', 'debug', '################ Detection d\'un trigger d\'un bouton d\'alerte ############');
+
+      log::add('seniorcarealertbt', 'debug', '################ Bouton d\'alerte déclenché ############');
 
       $seniorcarealertbt = seniorcarealertbt::byId($_option['seniorcarealertbt_id']); // on cherche la personne correspondant au bouton d'alerte
 
       foreach ($seniorcarealertbt->getConfiguration('action_alert_bt') as $action) { // pour toutes les actions définies
-        log::add('seniorcarealertbt', 'debug', 'Config Action bouton d\'alerte : ' . $action['action_label'] . ' - ' . $action['action_timer']);
+
+        log::add('seniorcarealertbt', 'debug', 'Config Action bouton d\'alerte - action_label : ' . $action['action_label'] . ' - action_timer : ' . $action['action_timer']);
 
         if(is_numeric($action['action_timer']) && $action['action_timer'] > 0){ // si on a un timer bien defini et > 0 min, on va lancer un cron pour l'execution retardée de l'action
-          //TODO : vérifier que CRON existe pas encore pour cette fonction et ce label et cette personne, si non : set CRON
-          // on vérifie que le cron existe pas encore pour ne pas retarder l'appel en cas de multi appui d'alerte. Et on veut pas non plus setter plusieurs CRON pour la meme action
-        }else{ // sinon, on execute tout de suite.
-          // C'est volontaire de ne pas vérifier que l'action a déjà été lancée, ca permet de relancer alerte si la personne redemande et aussi de lancer quand meme l'action si aucune action "annulation" n'est définie donc que le cache ne se remet jamais a 0...
+          // si le CRON existe deja, on ne l'update pas pour ne pas retarder l'alerte en cas de multi appui d'alerte. Et on veut pas non plus setter plusieurs CRON pour la meme action..
+
+          $cron = cron::byClassAndFunction('seniorcarealertbt', 'alertBtActionDelayed', array('eqLogic_id' => intval($seniorcarealertbt->getId()), 'action' => $action)); // cherche le cron qui correspond exactement à "ce plugin, cette fonction et ces options (personne, action (qui contient cmd, option (les titres et messages notamment) et label))" Si on change le label ou le message, c'est plus le meme "action" et donc cette fonction ne le trouve pas et un nouveau cron sera crée !
+          // lors d'une sauvegarde ou suppression de l'eqLogic, si des crons sont existants, ils seront supprimés avec un message d'alerte
+
+          if (!is_object($cron)) { // pas de cron trouvé, on le cree
+
+              $cron = new cron();
+              $cron->setClass('seniorcarealertbt');
+              $cron->setFunction('alertBtActionDelayed');
+
+              $options['eqLogic_id'] = intval($seniorcarealertbt->getId());
+              $options['action'] = $action; //inclu tout le detail de l'action : sa cmd, ses options pour les messages, son label, ...
+              $cron->setOption($options);
+
+              log::add('seniorcarealertbt', 'debug', 'Set CRON : ' . $options['eqLogic_id'] . ' - ' . $options['action']['cmd'] . ' - ' . $options['action']['action_label']);
+
+              $cron->setEnable(1);
+              $cron->setTimeout(5); //minutes
+
+              $delai = strtotime(date('Y-m-d H:i:s', strtotime('+'.$action['action_timer'].' min ' . date('Y-m-d H:i:s')))); // on lui dit de se déclencher dans 'action_timer' min
+              $cron->setSchedule(cron::convertDateToCron($delai));
+
+              $cron->setOnce(1); //permet qu'il s'auto supprime une fois executé
+              $cron->save();
+
+          } else {
+
+            log::add('seniorcarealertbt', 'debug', 'CRON existe deja pour : ' . $cron->getOption()['eqLogic_id'] . ' - ' . $cron->getOption()['action']['cmd'] . ' - ' . $cron->getOption()['action']['action_label'] . ' => on ne fait rien !');
+          }
+
+        }else{ // pas de timer valide defini, on execute l'action immédiatement
+          // C'est volontaire de ne pas vérifier que l'action a déjà été lancée, ca permet de relancer l'alerte immédiatement si la personne redemande et aussi de lancer quand meme l'action si aucune action "annulation" n'est définie donc que le cache ne se remet jamais a 0...
           // TODO : on pourrait quand meme mettre une tempo pour filtrer 40 appuis sur le bouton... à voir !
           log::add('seniorcarealertbt', 'debug', 'Pas de timer liée, on execute ' . $action['cmd']);
 
@@ -47,19 +88,17 @@ class seniorcarealertbt extends eqLogic {
 
       } // fin foreach toutes les actions
 
-    //  $seniorcarealertbt->execActions('action_alert_bt'); // on appelle les actions definies pour cette personne pour les boutons d'alertes
-
     }
 
-    public static function buttonAlertCancel($_option) { // fct appelée par le listener des buttons d'annulation d'alerte, n'importe quel bouton arrive ici
+    public static function buttonAlertCancel($_option) { // fct appelée par le listener des buttons d'annulation d'alerte, n'importe lequel des boutons d'annulation arrive ici
 
-      log::add('seniorcarealertbt', 'debug', '################ Detection d\'un trigger d\'un bouton d\'annulation d\'alerte ############');
+      log::add('seniorcarealertbt', 'debug', '################ Bouton d\'annulation d\'alerte déclenché ############');
 
-      $seniorcarealertbt = seniorcarealertbt::byId($_option['seniorcarealertbt_id']); // on cherche la personne correspondant au bouton d'alerte
+      $seniorcarealertbt = seniorcarealertbt::byId($_option['seniorcarealertbt_id']); // on cherche la personne correspondant au bouton
 
-      foreach ($seniorcarealertbt->getConfiguration('action_cancel_alert_bt') as $action) { // pour toutes les actions définies
+      foreach ($seniorcarealertbt->getConfiguration('action_cancel_alert_bt') as $action) { // pour toutes les actions d'annulation définies
 
-        $execActionLiee = $seniorcarealertbt->getCache('execAction_'.$action['action_label_liee']); // on la lire le cache d'execution de l'action liée
+        $execActionLiee = $seniorcarealertbt->getCache('execAction_'.$action['action_label_liee']); // on va lire le cache d'execution de l'action liée, savoir si deja lancé ou non...
 
         log::add('seniorcarealertbt', 'debug', 'Config Action Accusé Réception bouton d\'alerte, action : '. $action['cmd'] .', label action liée : ' . $action['action_label_liee'] . ' - action liée deja executée : ' . $execActionLiee);
 
@@ -69,7 +108,7 @@ class seniorcarealertbt extends eqLogic {
 
           $seniorcarealertbt->execAction($action);
 
-        }else if(isset($action['action_label_liee']) && $action['action_label_liee'] != '' && $execActionLiee == 1){ // si on a une action liée définie et qu'elle a été executée => on execute notre action et on remet le cache de l'action liée à 0 (fait uniquement pour les boutons d'annulation et non à la reception de l'AR)
+        }else if(isset($action['action_label_liee']) && $action['action_label_liee'] != '' && $execActionLiee == 1){ // si on a une action liée définie et qu'elle a été executée => on execute notre action et on remet le cache de l'action liée à 0 (fait uniquement pour les boutons d'annulation et non à la réception de l'AR, donc l'aidant ayant recu une alerte pourra recevoir l'info qu'il y a eu une AR (mais on sait pas par qui... TODO...) puis que l'alerte est résolue)
 
           log::add('seniorcarealertbt', 'debug', 'Action liée ('.$action['action_label_liee'].') executée précédemment, donc on execute ' . $action['cmd'] . ' et remise à 0 du cache d\'exec de l\'action origine');
 
@@ -77,47 +116,17 @@ class seniorcarealertbt extends eqLogic {
 
           $seniorcarealertbt->setCache('execAction_'.$action['action_label_liee'], 0);
 
-        }else{ // sinon, on log qu'on ne l'execute pas
+        }else{ // sinon, on log qu'on n'execute pas l'action et la raison
           log::add('seniorcarealertbt', 'debug', 'Action liée ('.$action['action_label_liee'].') non executée précédemment, donc on execute pas ' . $action['cmd']);
           //TODO : ecrire dans la doc que lier un label qui n'existe pas fait qu'on executera jamais l'action...
-        } //*/
+        }
 
       } // fin foreach toutes les actions
 
-      //TODO : couper les CRON des actions d'alertes non encore appelés
-
-
-
-    //  $seniorcarealertbt->execActions('action_cancel_alert_bt'); // on appelle les actions definies pour cette personne pour les boutons d'alertes
+      //coupe les CRON des actions d'alertes non encore appelés
+      $this->cleanAllCron();
 
     }
-
-
-/*    public static function cron() { //executée toutes les min par Jeedom
-
-      log::add('seniorcarealertbt', 'debug', '#################### CRON ###################');
-
-      //pour chaque equipement (personne) declaré par l'utilisateur
-      foreach (self::byType('seniorcarealertbt',true) as $seniorcarealertbt) {
-
-
-      } // fin foreach equipement
-
-    } //fin cron //*/
-
-    //*
-    // * Fonction exécutée automatiquement toutes les 15 minutes par Jeedom
-    // Sert ici pour les capteurs conforts
-/*      public static function cron15() {
-
-        log::add('seniorcarealertbt', 'debug', '#################### CRON 15 ###################');
-
-        //pour chaque equipement (personne) declaré par l'utilisateur
-        foreach (self::byType('seniorcarealertbt',true) as $seniorcarealertbt) {
-
-        } // fin foreach equipement
-
-      } //*/
 
     /*
      * Fonction exécutée automatiquement toutes les heures par Jeedom
@@ -142,7 +151,7 @@ class seniorcarealertbt extends eqLogic {
       log::add('seniorcarealertbt', 'debug', '################ Execution de l\' actions ' . $_config . ' pour ' . $this->getName() .  ' ############');
 
       try {
-        $options = array(); // va permettre d'appeler les options de configuration des actions, par exemple un scenario un message
+        $options = array(); // va permettre d'appeler les options de configuration des actions, par exemple un scenario ou les textes pour un message
         if (isset($action['options'])) {
           $options = $action['options'];
           foreach ($options as $key => $value) { // ici on peut définir les "tag" de configuration qui seront à remplacer par des variables
@@ -169,51 +178,16 @@ class seniorcarealertbt extends eqLogic {
       } catch (Exception $e) {
         log::add('seniorcarealertbt', 'error', $this->getHumanName() . __(' : Erreur lors de l\'éxecution de ', __FILE__) . $action['cmd'] . __('. Détails : ', __FILE__) . $e->getMessage());
 
-        if(isset($action['action_label'])){ // en cas d'erreur d'execution, on memorise qu'on a pas pu lancer l'action !
-          $this->setCache('execAction_'.$action['action_label'], 0);
-        }
-
       }
 
     }
 
-//TODO : sera a virer
-    public function execActions($_config, $_sensor_name = NULL, $_sensor_type = NULL, $_sensor_value = NULL) { // on donne le type d'action en argument et ca nous execute toute la liste. Les autres arguments sont pour les tag des messages si applicable
 
-      log::add('seniorcarealertbt', 'debug', '################ Execution des actions du type ' . $_config . ' pour ' . $this->getName() .  ' ############');
+    public function btAlertAR() { // fct appelée par la cmd action appelée par l'extérieur pour AR de l'alerte en cours
 
-      foreach ($this->getConfiguration($_config) as $action) { // on boucle pour executer toutes les actions définies
-        try {
-          $options = array(); // va permettre d'appeler les options de configuration des actions, par exemple un scenario un message
-          if (isset($action['options'])) {
-            $options = $action['options'];
-            foreach ($options as $key => $value) { // ici on peut définir les "tag" de configuration qui seront à remplacer par des variables
-              // str_replace ($search, $replace, $subject) retourne une chaîne ou un tableau, dont toutes les occurrences de search dans subject ont été remplacées par replace.
-              $value = str_replace('#senior_name#', $this->getConfiguration('senior_name'), $value);
-              $value = str_replace('#senior_tel#', $this->getConfiguration('senior_tel'), $value);
-              $value = str_replace('#senior_address#', $this->getConfiguration('senior_address'), $value);
+      log::add('seniorcarealertbt', 'debug', '################ Detection d\'un appel d\'Accusé de Réception ############');
 
-              $value = str_replace('#senior_ref_person#', $this->getConfiguration('senior_ref_person'), $value);
-              $value = str_replace('#senior_ref_person_tel#', $this->getConfiguration('senior_ref_person_tel'), $value);
-
-              $value = str_replace('#sensor_name#', $_sensor_name, $value);
-              $value = str_replace('#sensor_type#', $_sensor_type, $value);
-              $options[$key] = str_replace('#sensor_value#', $_sensor_value, $value);
-            }
-          }
-          scenarioExpression::createAndExec('action', $action['cmd'], $options);
-        } catch (Exception $e) {
-          log::add('seniorcarealertbt', 'error', $this->getHumanName() . __(' : Erreur lors de l\'éxecution de ', __FILE__) . $action['cmd'] . __('. Détails : ', __FILE__) . $e->getMessage());
-        }
-      } //*/
-
-    }
-
-    public function btAlertAR() { // fct appelée par la cmd action appelée par l'extérieur pour faire AR de l'alerte en cours
-
-      log::add('seniorcarealertbt', 'debug', '################ Detection de l\'appel AR ############');
-
-      foreach ($this->getConfiguration('action_ar_alert_bt') as $action) { // pour toutes les actions définies
+      foreach ($this->getConfiguration('action_ar_alert_bt') as $action) { // pour toutes les actions définies pour les AR
 
         $execActionLiee = $this->getCache('execAction_'.$action['action_label_liee']);
 
@@ -233,12 +207,34 @@ class seniorcarealertbt extends eqLogic {
 
         }else{ // sinon, on log qu'on ne l'execute pas
           log::add('seniorcarealertbt', 'debug', 'Action liée ('.$action['action_label_liee'].') non executée précédemment, donc on execute pas ' . $action['cmd']);
-          //TODO : ecrire dans la doc que lier un label qui n'existe pas fait qu'on executera jamais l'action...
-        } //*/
+        }
 
       } // fin foreach toutes les actions
 
-      //TODO : couper les CRON des actions d'alertes non encore appelés
+      //coupe les CRON des actions d'alertes non encore appelés
+      $this->cleanAllCron();
+
+    }
+
+    public function cleanAllCron($displayWarningMessage = false) {
+
+      log::add('seniorcarealertbt', 'debug', 'Fct cleanAllCron pour : ' . $this->getName());
+
+      $cron = cron::byClassAndFunction('seniorcarealertbt', 'alertBtActionDelayed'); //on cherche le 1er cron pour ce plugin et cette action (il n'existe pas de fonction core renvoyant un array avec tous les cron de la class, comme pour les listeners... dommage...)
+
+      while (is_object($cron) && $cron->getOption()['eqLogic_id'] == $this->getId()) { // s'il existe et que l'id correspond, on le vire puis on cherche le suivant et tant qu'il y a un suivant on boucle
+
+        log::add('seniorcarealertbt', 'debug', 'Cron trouvé à supprimer pour eqLogic_id : ' . $cron->getOption()['eqLogic_id'] . ' - cmd : ' . ' - ' . $cron->getOption()['action']['cmd'] . ' - action_label : ' . $cron->getOption()['action']['action_label']);
+
+        if($displayWarningMessage){
+
+          log::add('seniorcarealertbt', 'error', 'Attention, des actions d\'alerte avec un délai avant exécution sont en cours et vont être supprimées, merci de vous assurer que la personne associée n\'a pas besoin d\'assistance ! Il s\'agit de ' . $this->getConfiguration('senior_name') . ' - pour l\'eqLogic ' . $this->getName() . ', action supprimée : ' . $cron->getOption()['action']['cmd'] . ' - action_label : ' . $cron->getOption()['action']['action_label']);
+        }
+
+        $cron->remove();
+        $cron = cron::byClassAndFunction('seniorcarealertbt', 'alertBtActionDelayed');
+      }
+
 
     }
 
@@ -269,7 +265,7 @@ class seniorcarealertbt extends eqLogic {
       $cmd = $this->getCmd(null, 'alerte_bt_ar');
       if (!is_object($cmd)) {
         $cmd = new seniorcarealertbtCmd();
-        $cmd->setName(__('AR Bouton Alerte', __FILE__));
+        $cmd->setName(__('Accuser Réception Alerte', __FILE__));
       }
       $cmd->setLogicalId('alerte_bt_ar');
       $cmd->setEqLogic_id($this->getId());
@@ -277,17 +273,20 @@ class seniorcarealertbt extends eqLogic {
       $cmd->setSubType('other');
       $cmd->setIsVisible(0);
       $cmd->setIsHistorized(1);
-      $cmd->setConfiguration('historizeMode', 'none');
+      $cmd->setConfiguration('historizeMode', 'none'); //on garde en mémoire tous les AR recu. //TODO : voir comment on pourrait avoir l'info de "qui" a accusé réception...
       $cmd->save();
     }
 
     public function preSave() {
 
+      //supprime les CRON des actions d'alertes non encore appelés, affiche une alerte s'il y en avait
+      //sert à ne pas laisser trainer des CRONs en cours si on change le message ou le label puis en enregistre. Cas exceptionnel, mais au cas où...
+      $this->cleanAllCron(true);
+
     }
 
     // fct appellée par Jeedom aprés l'enregistrement de la configuration
     public function postSave() {
-
 
       //########## 1 - On va lire la configuration des capteurs dans le JS et on la stocke dans un grand tableau #########//
 
@@ -312,7 +311,6 @@ class seniorcarealertbt extends eqLogic {
       }
 
       //########## 2 - On boucle dans toutes les cmd existantes, pour les modifier si besoin #########//
-
 
       foreach ($jsSensors as $key => $jsSensor) { // on boucle dans tous nos différents types de capteurs. $key va prendre les valeurs suivantes : life_sign, alert_bt, confort puis security
 
@@ -371,7 +369,6 @@ class seniorcarealertbt extends eqLogic {
 
         } //*/ // fin foreach restant. A partir de maintenant on a des capteurs qui refletent notre config lue en JS
       }
-
 
       //########## 4 - Mise en place des listeners de capteurs pour réagir aux events #########//
 
@@ -445,10 +442,14 @@ class seniorcarealertbt extends eqLogic {
 
     }
 
-    public function preRemove() {
+    public function preRemove() { //quand on supprime notre eqLogic
 
-      // quand on supprime notre eqLogic, on vire nos listeners associés
+      // on vire nos listeners associés
       $this->cleanAllListener();
+
+      //supprime les CRON des actions d'alertes non encore appelés, affiche une alerte s'il y en avait
+      //sert à ne pas laisser trainer des CRONs en cours si on change le message ou le label puis en enregistre. Mais ne devrait arriver qu'exceptionnellement
+      $this->cleanAllCron(true);
 
     }
 
